fb4d525d95945ef0eed77eccf96995e300926fb0
tkocou <tkocou@protonmail.com>
2024-08-09 18:11:21 -0400

    update

--- ve_utilities.py
+++ ve_utilities.py
@@ -96,7 +96,7 @@ def make_launcher():
     ## read in the result and assign it to a variab;e
     with open("tmp_file","r") as x:
         shell_cmd = x.read()
-    shell_cmd.rstrip() ## last character is a '\n'; remove it
+    shell_cmd.rstrip('\n') ## last character is a '\n'; remove it
     print("shell_cmd: ",list(shell_cmd))
     ## remove the temporary file.
     os.remove(file_to_cap)
